5ad7d60464f270d7d9e02eb5cba62ba156fe81ab
George Walker <george.walker@byu.edu>
2014-10-02 11:37:25 -0600

    Commit everything!

---
+++ DateTimeParser/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DateTimeParser.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index(string parseThis)
+		{
+			if (String.IsNullOrEmpty(parseThis)) return View();
+			ViewBag.ParseThis = parseThis;
+			DateTime result;
+			if (DateTime.TryParse(parseThis, out result))
+			{
+				ViewBag.Output = result.ToLongDateString() + " " + result.ToLongTimeString();
+				ViewBag.Success = true;
+			}
+			else
+			{
+				ViewBag.Success = false;
+			}
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "About";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Questions?";
+
+			return View();
+		}
+	}
+}
